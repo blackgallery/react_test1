033c5087cf8864c042420f919b5c61c3ec1e76c2
Joni <pacificjoni@gmail.com>
2020-08-31 16:46:42 +0600

    Updates

---
+++ precache-manifest.d79387c150747c196787a502a2559c2f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51596bb08aaf5333573a24fe74a92132",
+    "url": "/react_test1/index.html"
+  },
+  {
+    "revision": "fafa5e82fe04915364a7",
+    "url": "/react_test1/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "42fae2e7568180233eaa",
+    "url": "/react_test1/static/js/2.d4752478.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_test1/static/js/2.d4752478.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fafa5e82fe04915364a7",
+    "url": "/react_test1/static/js/main.b69f2175.chunk.js"
+  },
+  {
+    "revision": "1b1d978a63296b00af4c",
+    "url": "/react_test1/static/js/runtime-main.23c26b81.js"
+  },
+  {
+    "revision": "d8a6563e630b75cc722f60594ff196e2",
+    "url": "/react_test1/static/media/about.d8a6563e.jpg"
+  }
+]);
